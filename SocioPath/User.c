ba73d2de4b39d2d7b5718a51610f1d93ab0f9922
amirmk89 <amirmk89@gmail.com>
2015-07-15 11:47:51 +0300

    started building User object.

--- SocioPath/User.c
+++ SocioPath/User.c
@@ -1,1 +1,28 @@
 #include "User.h"
+
+typedef struct user_t{
+	char username[USERNAME_LEN + 1];
+	char password[PASS_LEN+1];
+	int randomNum;
+	char securityAnswer[SECURITY_ANS_LEN+1];
+
+} User;
+
+char* SerializeUser(User *user){
+	
+	int stringSize = USERNAME_LEN + PASS_LEN + SECURITY_ANS_LEN + 3 * SEP_LEN+1;
+	char *ans = malloc(stringSize * sizeof(char));
+	char rand_num_char = user->randomNum + '0';
+
+	if (ans == NULL) return NULL; //is necessary?
+
+	strcat(ans, user->username);
+	strcat(ans, SEPERATOR);
+	strcat(ans, user->password);
+	strcat(ans, SEPERATOR);
+	strcat(ans, rand_num_char);
+	strcat(ans, SEPERATOR);
+	strcat(ans, user->securityAnswer);
+
+	return ans;
+}
---
+++ SocioPath/User.h
@@ -0,0 +1,13 @@
+#ifndef User_h
+#include <stdio.h>
+#include <string.h>
+
+#define User_h
+
+#define USERNAME_LEN 50
+#define PASS_LEN 8
+#define SECURITY_ANS_LEN 100
+#define SEPERATOR "_$_"
+#define SEP_LEN 3
+
+#endif
